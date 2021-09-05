df9c586cfb61b001305b515b5a2804a3c5430ee2
Caio Krauthamer <caio_k@hotmail.com>
2021-09-05 12:15:44 -0300

    Turn generate_spreadsheet into a proper script

--- scripts/generate_spreadsheet.py
+++ scripts/generate_spreadsheet.py
@@ -1,20 +1,20 @@
 import asyncio
+import argparse
 
 from scripts.common import write_items_to_excel
 from steamapi.inventory import get_user_inventory
 from steamapi.item_price import add_items_price
 
 
-async def main():
-    # variables to be threated as input in the future
-    steam_id = 76561198066658320
-    app_ids = [730]
-    item_names_language = "portuguese"
-    currency = 7  # reminder - with html mode only USD is possible
-    item_price_source = "html"  # reminder only html does not rate limit with used sleep time
-    item_price_retrieval_mode = "linear"  # reminder - parallel will rate limit you
-    excel_file_name = "cs_go_items_prices" + ".xlsx"
-
+async def main(
+    steam_id,
+    app_ids,
+    item_names_language,
+    currency,
+    item_price_source,
+    item_price_retrieval_mode,
+    excel_file_name
+):
     # get user's inventory
     user_items = []
     for app_id in app_ids:
@@ -38,4 +38,79 @@ async def main():
     write_items_to_excel(user_filtered_items, [], excel_file_name)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    # creates an argparse object to parse command line option
+    parser = argparse.ArgumentParser(description = "Build spreadsheet with an user's desired items")
+    parser.add_argument(
+        "steam_id",
+        help = "Users's Steam id (search for 'ID Steam' on 'https://store.steampowered.com/account')",
+        type = int,
+    )
+    parser.add_argument(
+        "--app_ids",
+        dest = "app_ids",
+        help = "App ids to retrieve items from. CSGO is 730. Check all at 'https://steamdb.info/'",
+        nargs = "+",
+        type = int,
+        default = [730],
+    )
+    parser.add_argument(
+        '--item_names_language',
+        dest = "item_names_language",
+        help = "Language to display item names ('portuguese' (default), 'english')",
+        type = str,
+        default = "portuguese",
+    )
+    parser.add_argument(
+        "--currency",
+        dest = "currency",
+        help = "Currency to retrive prices (default: 7 -> BRL). Reminder: 'html' fetch mode will only retrieve prices in USD",
+        type = int,
+        default = 7,
+    )
+    parser.add_argument(
+        "--item_price_source",
+        dest = "item_price_source",
+        help = "How to retrive prices. Options: overview, history or html. Note: use html always",
+        type = str,
+        default = "html",
+    )
+    parser.add_argument(
+        "--item_price_retrieval_mode",
+        dest = "item_price_retrieval_mode",
+        help = "How to trigger requests to steam API: linear (default) or parallel (will rate limit you)",
+        type = str,
+        default = "linear",
+    )
+    parser.add_argument(
+        "--excel_file_name",
+        dest = "excel_file_name",
+        help = "Which file name to use. Do not add extension to it, .xlxs will be used. 'prices' is the default value",
+        type = str,
+        default = "prices",
+    )
+
+    # waits for command line input
+    # (proceeds only if it is validated against the options set before)
+    args = parser.parse_args()
+
+    # validate provided input
+    if args.item_names_language not in ["english", "portuguese"]:
+        print("Invalid chosen language, choose either 'english' or 'portuguese'")
+        exit()
+    if args.item_price_source not in ["html", "overview", "history"]:
+        print("Invalid chosen language, choose either 'html', 'overview' or 'history'")
+        exit()
+    if args.item_price_retrieval_mode not in ["linear", "parallel"]:
+        print("Invalid chosen language, choose either 'linear' or 'parallel'")
+        exit()
+
+    # start async loop
+    asyncio.run(main(
+        args.steam_id,
+        args.app_ids,
+        args.item_names_language,
+        args.currency,
+        args.item_price_source,
+        args.item_price_retrieval_mode,
+        args.excel_file_name + ".xlsx"
+    ))
