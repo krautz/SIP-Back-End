3ddeb0a486295460695c2d4413e67d56c9ea6257
Caio Krauthamer <caio_k@hotmail.com>
2021-09-05 13:45:14 -0300

    Group tradeable and non-tradeable items on user inventory

--- steamapi/inventory.py
+++ steamapi/inventory.py
@@ -23,22 +23,26 @@ async def get_user_inventory(steam_user_id, app_id, language = "english"):
 
     # get user tradable items
     user_items = {}
+    class_id_to_hash_name = {}
     for api_item in user_items_api_response["descriptions"]:
         if api_item["marketable"] == 1:
             item_class_id = api_item["classid"]
-            user_items[item_class_id] = {
+            item_market_hash_name = api_item["market_hash_name"]
+            user_items[item_market_hash_name] = {
                 "amount": 0,  # amount is present on api response assets. just initializing it.
                 "app_id": api_item["appid"],  # game code that has the item
-                "market_hash_name": api_item["market_hash_name"],  # item 'id' to requst price later
+                "market_hash_name": item_market_hash_name,  # item 'id' to requst price later
                 "name": api_item["market_name"],  # item human friendly name
             }
+            class_id_to_hash_name[item_class_id] = item_market_hash_name
 
     # get amount of each user items
     for api_asset in user_items_api_response["assets"]:
         item_class_id = api_asset["classid"]
+        item_market_hash_hame = class_id_to_hash_name.get(item_class_id)
 
         # skip items not present in user items (i.e. not tradable ones)
-        if user_item := user_items.get(item_class_id):
-            user_item["amount"] += int(api_asset["amount"])
+        if item_market_hash_hame:
+            user_items[item_market_hash_hame]["amount"] += int(api_asset["amount"])
 
     return list(user_items.values())
