758d5a6fb27b9382690477b3dd3c70ecc6dbadc4
Caio Krauthamer <caio_k@hotmail.com>
2024-02-16 19:37:07 -0300

    Update SteamItemsAPIException to have an 'extra' property to help on debugging

--- src/external_apis/steam/exceptions.py
+++ src/external_apis/steam/exceptions.py
@@ -1,9 +1,17 @@
 class SteamItemsAPIException(Exception):
-    def __init__(self, name: str, market_hash_name: str, status_code: str):
+    def __init__(self, name: str, market_hash_name: str, status_code: str, extra: str | None = None):
         self.name = name
         self.market_hash_name = market_hash_name
         self.status_code = status_code
-        self.message = f"Error retrieving price for {self.name} ({self.market_hash_name}) - Status: {self.status_code}"
+        if extra is not None:
+            self.message = (
+                f"Error retrieving price for {self.name} ({self.market_hash_name}) - Status: {self.status_code}"
+                f"Extra: {extra}"
+            )
+        else:
+            self.message = (
+                f"Error retrieving price for {self.name} ({self.market_hash_name}) - Status: {self.status_code}"
+            )
         super().__init__(self.message)
 
     def log(self):
